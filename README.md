fee680151c2e32c8efad6c398daf242b74186d1f
Fabrizio Musacchio <fabrizio.musacchio@posteo.de>
2022-06-28 23:48:06 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,57 @@
+# DEVONthink Markdown TOC generator
+
+This Applescript generates a table of contents (TOC) and Back-to-top links in DEVONthink's Markdown documents.
+
+<div style="text-align: center;"><a href="Screenshot_preview.png"><img src="Screenshot_preview.png" style="width: 49%;"></a>
+<a href="Screenshot_editview.png"><img src="Screenshot_editview.png" style="width: 49%;"></a>
+
+The generated TOC in DEVONthink's Markdown preview (left) and edit (right) view. 
+</div> 
+
+## How to install
+Download the two Applescript and Python files:
+
+* TOC generate.scpt
+* TOC remove.scpt
+* generate_toc.py
+* remove_toc.py
+
+Place the Applescript files into DEVONthink's script folder.
+
+<div style="text-align: center;"><a href="Screenshot_DEVONthink_script_folder.png"><img src="Screenshot_DEVONthink_script_folder.png" style="width: 100%;"></a>
+
+Open DEVONthink's script folder via DEVONthink's script menu.
+</div> 
+
+Place the Python scripts to a folder of your choice. The Applescripts rely on these Python scripts, which work for both Python 2 and 3. The path to the folder containing the Python scripts must be specified in the Applescripts (in the `MyPythonScriptPath` variable).
+
+
+## How the script works
+The Applescript hands-over the document's content to the Python script, which scans for all lines starting with a `#`, collects the heading texts and adds a label at the end of each headline (e.g., `## Section 1 [h0]`). The labels – not visible in the preview mode – are required for a simple but unambiguous linking to the headlines and are automatically updated with each run of the script. A Back-to-top link is placed over each headline, which links to the TOC. 
+
+The script also scans for an existing TOC. If available, that TOC is updated and its previous position in the document is retained. I.e., after the first run of the script, a TOC section (`<!-- TOC START -->...<!-- TOC END -->`) is inserted right after the first line of the document, and you can freely move that section within the document.
+
+In the Python script, you can define the TOC-level to be considered by adjusting the `toc_level` variable:
+
+```
+toc_level defines at which toc-level to start, e.g.:
+  2 - only account for headings h2 (##) and higher; requires that
+      the first occuring heading is h2 (except the document
+      heading in line 0 (headline_index))
+  1 - account for headings h1 (#) and higher; requires that a
+      the first occuring heading is h1 (except the document
+      heading in line 0 (headline_index))
+```
+
+By default, the script ignores the first line of the document, so that the document's title will not be part of the TOC. 
+
+The script respects a so-called reference
+links section (`<!-- LINKS START -->...<!-- LINKS END -->?`, if available). This section is the result of another Applescript I apply for handling Markdown reference links.
+
+When the Python script is done, the original content including the TOC and all other additions is hand-overed back to the Applescript, which replaces the document's current content with the updated one.
+
+The "TOC remove" script removes the generated TOC, the added headline labels and the Back-to-top links.
+
+## Use at your own risk
+I checked both scripts for various possible formatting within a Markdown document and I tried to make them as failure-resistant as possible. Nevertheless, the scripts replace your document's content with an actually new version of the content, which contains the additions mentioned above. In extreme cases, formatting that have not yet been taken into account can change the content. **Therefore, please backup your files before you apply the scripts and first try them out with some Markdown files, that contain the some formatting that you usually apply to your documents**. Please report any bugs in the issue section.
+
---
+++ generate_toc.py
@@ -0,0 +1,111 @@
+"""
+This script generates a TOC and Back-to-top links in Markdown documents.
+It scans for an existing TOC (and if available, updates it and keeps the
+position of that existing TOC) and also respects the so-called reference
+links section (if any, "<!-- LINKS START -->...<!-- LINKS END -->?").
+
+The script is tailored to work in combination with an Applescript to add
+the TOC to DEVONthink Markdown documents.
+
+author: Fabrizio Musacchio (hello@fabriziomusacchio.com)
+date:  19.02.2022
+"""
+# %% IMPORTS
+import sys
+import re
+text = sys.argv[1]
+# %% DEFINE TOC-LEVEL
+toc_level = 2
+"""
+define at which toc-level to start, e.g.:
+  2 - only account for headings h2 (##) and higher; requires that
+      the first occurring heading is h2 (except the document
+      heading in line 0)
+  1 - account for headings h1 (#) and higher; requires that a
+      the first occurring heading is h1 (except the document
+      heading in line 0)
+"""
+# %% MAIN ROUTINE
+Current_Markdown_Text = sys.argv[1]
+"""
+    The following text conversion and re-conversion has to be performed,
+    as the shell echo of multiline text within the Applescript introduces
+    a a type of linebreaks other than "\n", which is not interpretable by
+    Python. The following procedure re-introduces the "\n"-linebreaks:
+"""
+Current_Markdown_New_Text = Current_Markdown_Text.splitlines()
+Current_Markdown_New_Text_tmp = [Current_Markdown_New_Text[0]]
+# Current_Markdown_New_Text_tmp.append(Current_Markdown_New_Text[0])
+
+# Scan the text for "#" and collect the TOC entries:
+headline_index = 0
+TOC_collector_list = []
+for line_idx, line in enumerate(Current_Markdown_New_Text[1:]):
+    line_idx+=1
+    """
+    line_idx starts with the value 1 and therefore the first line of the document 
+    (the document's title) is skipped.
+    """
+    if line.startswith("#"):
+        # remove any id in headlines from a previous run:
+        line_cleaned = re.sub(r'\s*\[.*\]', '', line)
+
+        # construct the new TOC entry:
+        current_heading = re.sub(r'(^\#*\s)', '', line_cleaned)
+        current_toc_entry=len(re.findall(r'[\#]', line_cleaned[toc_level:]))*("\t") + '1. ' + '['+current_heading+']'
+        current_toc_link = '(#h'+str(headline_index)+')'
+        TOC_collector_list.append(current_toc_entry+current_toc_link)
+
+        # update the current heading and add the id and the Back-to-top link:
+        newlines = '\n\n'
+        Current_Markdown_New_Text_tmp.append("<div style='text-align: right;'><a href='#toc'>Toc &#8593;</a></div>" + newlines +
+                                             line_cleaned + ' [h'+str(headline_index)+']')
+        headline_index += 1
+    elif line=="<div style='text-align: right;'><a href='#toc'>Toc &#8593;</a></div>":
+        # remove (by skipping) any existing Back-to-top link and some whitespace:
+        if Current_Markdown_New_Text_tmp[-1] == '':
+            del Current_Markdown_New_Text_tmp[-1]
+    else:
+        Current_Markdown_New_Text_tmp.append(line)
+
+# scan for existing TOC and link sections:
+toc_start_idx = 0
+toc_end_idx   = 0
+link_start_idx= 0
+for line_idx, line in enumerate(Current_Markdown_New_Text_tmp):
+    if line == '<!-- TOC START -->':
+        toc_start_idx = line_idx
+    if line == '<!-- TOC END -->':
+        toc_end_idx = line_idx
+    if line == '<!-- LINKS START -->':
+        link_start_idx= line_idx
+
+# insert the TOC:
+if toc_start_idx==0 and toc_end_idx==0:
+    Current_Markdown_New_Text_tmp2 = []
+    Current_Markdown_New_Text_tmp2+=[Current_Markdown_New_Text_tmp[0]]
+    Current_Markdown_New_Text_tmp2.append('\n<!-- TOC START -->')
+    Current_Markdown_New_Text_tmp2.append("<br><a name='toc'></a>\n\n---")
+    for toc_entry in TOC_collector_list:
+        Current_Markdown_New_Text_tmp2.append(toc_entry)
+    Current_Markdown_New_Text_tmp2.append('\n---\n<!-- TOC END -->')
+else:
+    Current_Markdown_New_Text_tmp2 = []
+    Current_Markdown_New_Text_tmp2+=Current_Markdown_New_Text_tmp[0:toc_start_idx]
+    Current_Markdown_New_Text_tmp2.append('<!-- TOC START -->')
+    Current_Markdown_New_Text_tmp2.append("<br><a name='toc'></a>\n\n---")
+    for toc_entry in TOC_collector_list:
+        Current_Markdown_New_Text_tmp2.append(toc_entry)
+    Current_Markdown_New_Text_tmp2.append('\n---\n<!-- TOC END -->')
+if link_start_idx==0:
+    Current_Markdown_New_Text_tmp2 += Current_Markdown_New_Text_tmp[toc_end_idx+1:]
+else:
+    Current_Markdown_New_Text_tmp2 += Current_Markdown_New_Text_tmp[toc_end_idx + 1:link_start_idx]
+    Current_Markdown_New_Text_tmp2.append("<div style='text-align: right;'><a href='#toc'>Toc &#8593;</a></div>\n")
+    Current_Markdown_New_Text_tmp2 += Current_Markdown_New_Text_tmp[link_start_idx:]
+
+# rejoin the processed text:
+Current_Markdown_New_Text = '\n'.join(Current_Markdown_New_Text_tmp2)
+
+# echo it back to the shell:
+print(Current_Markdown_New_Text)
